bbeff5daf7c3adae422779c2d767a996cfeafa4d
Anastasios Poursaitedes <tasos@Anastasioss-MacBook-Pro.local>
2025-01-19 19:33:40 +0200

    Set up HomeComponent layout

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
+import { PhonebookComponent } from '../phonebook/phonebook.component';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ToolbarComponent, PhonebookComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
